6d858a4e940090af34e6dc9b8ae2840673a7556c
borgepl <pdeborger@gmail.com>
2021-12-07 14:42:18 +0100

    Angular first test app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myfirst-angular-project';
+  title = 'This is my first Application !';
+  firstName = 'Pascal';
+  lastName = 'De Borger';
 }
